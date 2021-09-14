9d03cd5b0d67efa4579a8e404d1fd7de3c29b7d8
zhih <fufong79570@gmail.com>
2021-09-15 00:04:22 +0800

    change api url

--- src/main.js
+++ src/main.js
@@ -15,15 +15,18 @@ new Vue({
 }).$mount('#app')
 
 if (token) {
-  Vue.use(new VueSocketIo({
-    // debug: true,
-    connection: 'https://twitter-project-2021.herokuapp.com/',
-    // connection: 'http://3e331a904c8b.ngrok.io',
-    options: {
-      query: {
-        auth: token
-      }
-    },
-    vuex: {}
-  }))
+  Vue.use(
+    new VueSocketIo({
+      // debug: true,
+      connection:
+        'http://ec2-52-198-213-230.ap-northeast-1.compute.amazonaws.com:3000',
+      // connection: 'http://3e331a904c8b.ngrok.io',
+      options: {
+        query: {
+          auth: token
+        }
+      },
+      vuex: {}
+    })
+  )
 }
